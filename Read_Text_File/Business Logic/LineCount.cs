940bd6cb853126e11532eeff9d8b7d8c7aafe79d
swastik-srvt <114081047+swastik-srvt@users.noreply.github.com>
2022-10-04 10:01:30 +0530

    Update LineCount.cs

--- Read_Text_File/Business Logic/LineCount.cs
+++ Read_Text_File/Business Logic/LineCount.cs
@@ -10,14 +10,14 @@ namespace Read_Text_File.Classes
     internal class LineCount : ICountData
     {
        
-        public int Get_Count_By_Query(List<string> each_file_line)
+        public int GetCountByQuery(List<string> eachFileLine)
         {
-            return (from line in each_file_line
+            return (from line in eachFileLine
                     select line).Count();
         }
-        public int Get_Count_By_Lamda(List<String> each_file_line)
+        public int GetCountByLamda(List<String> eachFileLine)
         {
-            return each_file_line.Select(lines => lines).Count();
+            return eachFileLine.Select(lines => lines).Count();
             
         }
     }
